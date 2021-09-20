f27b0c65ac98fea44a0112fd26411b00fa1c5bf1
Julien Pleijte <julienmaster21@gmail.com>
2021-09-20 04:28:20 +0200

    Add navbar

---
+++ src/Controller/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('pages/home.html.twig', []);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('pages/contact.html.twig', []);
+    }
+}
